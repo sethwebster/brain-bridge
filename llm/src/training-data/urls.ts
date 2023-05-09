a43549905dbb572a90c29197e5706368279a6b11
sethwebster <sethwebster@gmail.com>
2023-05-08 20:05:38 -0400

    Allows urls.ts to be included

---
+++ llm/src/training-data/urls.ts
@@ -0,0 +1,8 @@
+// This file contains the urls that will be used to train the model.
+const urls = [
+  // Url 1
+  "https://www.sethwebsterphotography.com/blog",
+  // Url 2 etc.
+]
+
+export default urls; 
