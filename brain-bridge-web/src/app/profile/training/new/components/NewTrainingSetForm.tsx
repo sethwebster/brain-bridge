898360c9486cec493dd56fbaedaa865e9b7d57d8
sethwebster <sethwebster@gmail.com>
2023-05-14 10:06:33 -0400

    fix typescript error

--- brain-bridge-web/src/app/profile/training/new/components/NewTrainingSetForm.tsx
+++ brain-bridge-web/src/app/profile/training/new/components/NewTrainingSetForm.tsx
@@ -13,6 +13,7 @@ export default function NewTrainingSetForm({
     sources: [],
     version: 0,
     dateCreated: new Date(),
+    prompt: "",
   });
   return <div>
     <input className="w-full p-2 mt-2 border rounded-md bg-slate-700 border-slate-600" alt="Training Set Name" placeholder="Training Set Name" type="text" name="name" value={trainingSetData.name} onChange={(e) => setTrainingSetData({...trainingSetData, name: e.target.value})} />
