d6dc634f5b82652f1a3154fea99c819b18b96770
sethwebster <sethwebster@gmail.com>
2023-06-02 12:47:17 -0400

    bind to 0.0.0.0

--- brain-bridge-server/src/index.ts
+++ brain-bridge-server/src/index.ts
@@ -6,10 +6,12 @@ import server from "./api";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-server.listen(process.env.API_PORT || "4141", () => {
+const port = parseInt(process.env.API_PORT || "4141", 10);
+
+
+server.listen(port, "0.0.0.0", () => {
   console.log(
-    `The API server has successfully started. \nListening at ${
-      process.env.APP_BASE_URL || `http://localhost:${process.env.API_PORT || "4141"}`
+    `The API server has successfully started. \nListening at ${process.env.APP_BASE_URL || `http://0.0.0.0:${process.env.API_PORT || "4141"}`
     }`
   );
 });
