67f6764e0234fea5a6e769681fd2c220849fc748
sethwebster <sethwebster@gmail.com>
2023-06-04 15:26:10 -0400

    Move options below prompt

--- brain-bridge-t3/src/app/(general)/profile/training/components/TrainingSetForm.tsx
+++ brain-bridge-t3/src/app/(general)/profile/training/components/TrainingSetForm.tsx
@@ -344,44 +344,6 @@ function TrainingSetForm({
           label="Use custom prompt"
           onChange={handleUseOwnPromptToggle}
         />
-        <div>
-          <h3 className="text-lg">Training Options</h3>
-          <div className="mb-4 flex flex-col">
-            <label className="text-sm">Maximum Segment Length</label>
-            <Input
-              className="p-2"
-              disabled={!canEdit}
-              type="text"
-              name="maxSegmentLength"
-              value={trainingOptions.maxSegmentLength}
-              placeholder="Enter a maximum segment length. Default is 2000."
-              onChange={(e) =>
-                handleTrainingOptionChanged("maxSegmentLength", e)
-              }
-            />
-            <small>
-              This is the maximum length of each document segment when split.
-            </small>
-          </div>
-          <div className="flex flex-col">
-            <label className="text-sm">Document Segment Overlap</label>
-            <Input
-              className="p-2"
-              disabled={!canEdit}
-              type="text"
-              name="maxSegmentLength"
-              value={trainingOptions.overlapBetweenSegments}
-              placeholder="Enter a desired document overlap. Default is 200."
-              onChange={(e) =>
-                handleTrainingOptionChanged("overlapBetweenSegments", e)
-              }
-            />
-            <small>
-              This is the amount of overlap between each document segment when
-              split.
-            </small>
-          </div>
-        </div>
       </div>
       {!trainingSetData.useOwnPrompt && !isNew && (
         <div>
@@ -420,6 +382,42 @@ function TrainingSetForm({
           disabled={!trainingSetData.useOwnPrompt}
         />
       )}
+      <div>
+        <h3 className="text-lg">Training Options</h3>
+        <div className="mb-4 flex flex-col">
+          <label className="text-sm">Maximum Segment Length</label>
+          <Input
+            className="p-2"
+            disabled={!canEdit}
+            type="text"
+            name="maxSegmentLength"
+            value={trainingOptions.maxSegmentLength}
+            placeholder="Enter a maximum segment length. Default is 2000."
+            onChange={(e) => handleTrainingOptionChanged("maxSegmentLength", e)}
+          />
+          <small>
+            This is the maximum length of each document segment when split.
+          </small>
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm">Document Segment Overlap</label>
+          <Input
+            className="p-2"
+            disabled={!canEdit}
+            type="text"
+            name="maxSegmentLength"
+            value={trainingOptions.overlapBetweenSegments}
+            placeholder="Enter a desired document overlap. Default is 200."
+            onChange={(e) =>
+              handleTrainingOptionChanged("overlapBetweenSegments", e)
+            }
+          />
+          <small>
+            This is the amount of overlap between each document segment when
+            split.
+          </small>
+        </div>
+      </div>
       <Sources
         disabled={!canEdit}
         trainingSetId={trainingSetData.id}
