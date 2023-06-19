cd166012a78e00f36d097617f1bec0a3e7199fab
sethwebster <sethwebster@gmail.com>
2023-06-19 17:52:04 -0400

    try again

--- brain-bridge-t3/tailwind.config.ts
+++ brain-bridge-t3/tailwind.config.ts
@@ -9,17 +9,10 @@ export default {
     require('@tailwindcss/typography')
   ],
   safelist: [
-    "grid-cols-1",
-    "grid-cols-2",
-    "grid-cols-3",
-    "grid-cols-4",
-    "grid-cols-5",
-    "grid-cols-6",
-    "grid-cols-7",
-    "grid-cols-8",
-    "grid-cols-9",
-    "grid-cols-10",
-    "grid-cols-11",
-    "grid-cols-12",
+    {
+    pattern: /grid-cols-.*/,
+    variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+    }
+    
   ]
 } satisfies Config;
