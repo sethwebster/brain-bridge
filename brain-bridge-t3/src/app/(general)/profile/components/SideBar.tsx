db9427e7bdfab9755553f1b7bf8a60f994b58ef8
sethwebster <sethwebster@gmail.com>
2023-06-26 19:40:23 -0400

    more ui tweaks

--- brain-bridge-t3/src/app/(general)/profile/components/SideBar.tsx
+++ brain-bridge-t3/src/app/(general)/profile/components/SideBar.tsx
@@ -1,5 +1,27 @@
+"use client";
 import Link from "next/link";
-import { MdChat, MdChatBubbleOutline, MdDashboard, MdReport, MdTableChart } from "react-icons/md";
+import {
+  MdChat,
+  MdChatBubbleOutline,
+  MdDashboard,
+  MdReport,
+  MdSummarize,
+  MdTableChart,
+} from "react-icons/md";
+
+function SideBarItemLabel({
+  children,
+  title,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <span className="flex-1 ml-3 opacity-0 whitespace-nowrap sm:opacity-90" title={title}>
+      {children}
+    </span>
+  );
+}
 
 export default function SideBar({
   setCount,
@@ -13,14 +35,14 @@ export default function SideBar({
   currentCosts: number;
 }) {
   return (
-    <div className="w-0 h-full overflow-hidden transition-all sm:w-64">
+    <div className="w-12 h-full overflow-hidden transition-all sm:w-64">
       {/* SIDEBAR */}
       <aside
         id="default-sidebar"
         className="sticky w-64 h-full shadow top-20 bg-gray-50 dark:bg-gray-800"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto">
+        <div className="h-full px-2 py-4 overflow-y-auto">
           <ul className="space-y-2 font-medium">
             <li>
               <Link
@@ -28,7 +50,7 @@ export default function SideBar({
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
                 <MdDashboard />
-                <span className="ml-3">Home</span>
+                <SideBarItemLabel title="Home">Home</SideBarItemLabel>
               </Link>
             </li>
             <li>
@@ -37,7 +59,7 @@ export default function SideBar({
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
                 <MdChat />
-                <span className="flex-1 ml-3 whitespace-nowrap">Chats</span>
+                <SideBarItemLabel title="Private Chats">Chats</SideBarItemLabel>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   {chatCount}
                 </span>
@@ -49,9 +71,9 @@ export default function SideBar({
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
                 <MdTableChart />
-                <span className="flex-1 ml-3 whitespace-nowrap">
+                <SideBarItemLabel title="Training Sets">
                   Training Sets
-                </span>
+                </SideBarItemLabel>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   {setCount}
                 </span>
@@ -63,9 +85,9 @@ export default function SideBar({
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
                 <MdChatBubbleOutline />
-                <span className="flex-1 ml-3 whitespace-nowrap">
+                <SideBarItemLabel title="Public Chats">
                   Public Chats
-                </span>
+                </SideBarItemLabel>
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   {publicChatCount}
                 </span>
@@ -76,10 +98,10 @@ export default function SideBar({
                 href="/profile/billing"
                 className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
-                <MdReport />
-                <span className="flex-1 ml-3 whitespace-nowrap">Billing</span>
+                <MdSummarize />
+                <SideBarItemLabel title="Billing">Billing</SideBarItemLabel>
                 <span className="inline-flex items-center justify-center w-auto h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  ${(currentCosts).toFixed(2)}
+                  ${currentCosts.toFixed(2)}
                 </span>
               </Link>
             </li>
