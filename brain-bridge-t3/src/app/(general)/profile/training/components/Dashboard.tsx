d0b41ea8fce5d5043a5764b7aee234f2655e5cdd
sethwebster <sethwebster@gmail.com>
2023-06-26 20:15:38 -0400

    fixed dashboard

--- brain-bridge-t3/src/app/(general)/profile/training/components/Dashboard.tsx
+++ brain-bridge-t3/src/app/(general)/profile/training/components/Dashboard.tsx
@@ -1,6 +1,42 @@
 import { type PublicChatInstance } from "@prisma/client";
 import { useMemo } from "react";
+import {
+  MdChat,
+  MdConstruction,
+  MdPublic,
+  MdQuestionAnswer,
+  MdQuestionMark,
+} from "react-icons/md";
 import { type TrainingSetWithRelations } from "~/data/interfaces/types";
+
+function DashboardCard({
+  amount,
+  icon,
+  title,
+  className,
+}: {
+  amount: number;
+  icon: React.ReactNode;
+  title: string;
+  className?: string;
+}) {
+  const resolvedClassName = className?.includes("bg-")
+    ? className
+    : (className ?? "") + " bg-white dark:bg-gray-800";
+  return (
+    <div
+      className={`flex flex-col items-center justify-center space-y-2 rounded-lg  p-4 shadow  ${
+        resolvedClassName ?? ""
+      }`}
+    >
+      <div className="text-3xl text-gray-700 dark:text-gray-200">{amount}</div>
+      <div className="flex flex-row items-center justify-center space-x-2 text-gray-500 dark:text-gray-200">
+        {icon}
+        <div className="text-sm">{title}</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Dashboard({
   trainingSet,
@@ -17,51 +53,49 @@ export default function Dashboard({
 
   return (
     <>
-      <div className="p-4">
-        <div className="rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
-          <div className="mb-4 grid grid-cols-3 gap-4">
-            <div className="flex h-24 items-center justify-center rounded bg-green-400 shadow dark:bg-gray-800">
-              <div className="flex flex-col text-center text-2xl text-gray-50 dark:text-gray-500">
-                <div>{trainingSet.conversations.length}</div>
-                <small>🔒 Active Conversations</small>
-              </div>
-            </div>
-            <div className="flex h-24 items-center justify-center rounded bg-green-400 shadow dark:bg-gray-800">
-              <div className="flex flex-col text-center text-2xl text-gray-50 dark:text-gray-500">
-                <div>{(trainingSet.publicChats || []).length}</div>
-                <small>📢 Public Chats</small>
-              </div>
-            </div>
-            <div
-              className={`flex h-24 items-center justify-center rounded ${
+      <div className="mt-4">
+        <div className="border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+          <div className="grid grid-cols-1 gap-4 mb-4 sm:grid-cols-3">
+            <DashboardCard
+              amount={trainingSet.conversations.length}
+              icon={<MdChat />}
+              title="Active"
+            />
+            <DashboardCard
+              amount={trainingSet.publicChats.length}
+              icon={<MdPublic />}
+              title="Public Chats"
+            />
+            <DashboardCard
+              amount={missedQuestionsCount}
+              icon={<MdConstruction />}
+              title="Unanswered"
+              className={
                 missedQuestionsCount === 0
-                  ? "bg-green-400"
+                  ? "bg-green-400 text-slate-50 dark:bg-green-600"
                   : missedQuestionsCount <= 3
-                  ? "bg-amber-400"
+                  ? "bg-yellow-400"
                   : "bg-red-400"
-              } shadow dark:bg-gray-800`}
-            >
-              <div className="flex flex-col text-center text-2xl text-gray-50 dark:text-gray-500">
-                <div>{missedQuestionsCount}</div>
-                <small>🚧 Unanswered Missed Questions</small>
-              </div>
-            </div>
-            <div className="flex h-24 items-center justify-center rounded bg-green-400 shadow dark:bg-gray-800">
-              <div className="flex flex-col text-center text-2xl text-gray-50 dark:text-gray-500">
-                <div>
-                  {trainingSet.publicChats.reduce(
-                    (acc, curr) =>
-                      (
-                        curr as unknown as {
-                          publicChatInstance: PublicChatInstance[];
-                        }
-                      ).publicChatInstance.length + acc,
-                    0
-                  )}
-                </div>
-                <small>Active Public Conversations</small>
-              </div>
-            </div>
+              }
+            />
+            <DashboardCard
+              amount={trainingSet.missedQuestions.length}
+              icon={<MdQuestionMark />}
+              title="Total Questions"
+            />
+            <DashboardCard
+              title="Active Public"
+              amount={trainingSet.publicChats.reduce(
+                (acc, curr) =>
+                  (
+                    curr as unknown as {
+                      publicChatInstance: PublicChatInstance[];
+                    }
+                  ).publicChatInstance.length + acc,
+                0
+              )}
+              icon={<MdPublic color="lightgreen" />}
+            />
           </div>
         </div>
       </div>
