9a630a0fd622d1de6c7588f5dc366054c6f0e593
sethwebster <sethwebster@gmail.com>
2023-07-09 08:23:17 -0400

    Adds warning on profile page to configure settings

--- brain-bridge-web/src/app/(general)/profile/page.tsx
+++ brain-bridge-web/src/app/(general)/profile/page.tsx
@@ -1,10 +1,24 @@
 import DismissableInfoBox from "~/app/components/DismissableInfoBox";
 import SideBarPaddedContainer from "./components/SidebarPaddedContainer";
+import { getServerSession } from "~/server/auth";
+import ServerData from "~/server/server-data";
+import InfoBox from "~/app/components/InfoBox";
+import Link from "next/link";
 
-export default function Profile() {
+export default async function Profile() {
+  const session = await getServerSession();
+  const userSettings = await ServerData.fetchUserSettings();
+
   return (
     <SideBarPaddedContainer>
       <div className="p-6">
+        {!userSettings && (
+          <InfoBox type="warning" title="Some configuration is required">
+            <p>
+              You have not yet configured your profile. Please go to the <Link href="/profile/settings" className="font-bold text-amber-700">settings page</Link> to configure your profile.
+            </p>
+          </InfoBox>
+        )}
         <DismissableInfoBox type="info" title="Profile" body="This is the profile page." dismissable dismissableId="profile-page" />
       </div>
     </SideBarPaddedContainer>
